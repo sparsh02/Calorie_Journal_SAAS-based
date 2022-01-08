bfa4280c3983e8d08509378e9da880c909164636
sparsh02 <sparshmishra02@gmail.com>
2022-01-08 11:46:17 +0530

    worked on client

--- server/routes/calorie.routes.js
+++ server/routes/calorie.routes.js
@@ -25,3 +25,32 @@ router.route("/add").post((req, res) => {
         .then(() => res.json("Calories Added Successfully"))
         .catch((err) => res.status(400).json("Error: " + err));
 });
+
+router.route("/:id").get((req, res) => {
+    Calorie.findById(req.params.id)
+        .then((calories) => res.json(calories))
+        .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+    Calorie.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Calories is deleted Successfully"))
+        .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+    Calorie.findById(req.params.id)
+        .then((calories) => {
+            calories.username = req.body.username;
+            calories.description = req.body.description;
+            calories.calories = Number(req.body.calories);
+            calories.date = Date.parse(req.body.date);
+            calories
+                .save()
+                .then(() => res.json("Calorie Updated Successfully"))
+                .catch((err) => res.status(400).json("Err: " + err));
+        })
+        .catch((err) => res.status(400).json("Err: " + err));
+});
+
+module.exports = router;
